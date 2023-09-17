13b709a7f1cf88ff155ebb5365a5677c14f3c398
Michael Bagnall <mrbagnall@icloud.com>
2023-09-17 16:00:38 -0500

    Fix some PHP 8.2 deprecations that will error in PHP 9

--- rdb/ValuedQuery/RVar.php
+++ rdb/ValuedQuery/RVar.php
@@ -7,20 +7,21 @@ use r\ValuedQuery\ValuedQuery;
 use r\Datum\NumberDatum;
 use r\ProtocolBuffer\TermTermType;
 
-class RVar extends ValuedQuery
-{
+class RVar extends ValuedQuery {
+
+    public string $name;
 
     private $id;
 
     private static $nextVarId = 1;
 
-    public function __construct($name)
+    public function __construct($variableName)
     {
-        if (!is_string($name)) {
+        if (!is_string($variableName)) {
             throw new RqlDriverError("Variable name must be a string.");
         }
         $this->id = RVar::$nextVarId;
-        $this->name = $name;
+        $this->name = $variableName;
 
         if (RVar::$nextVarId == (1 << 31) - 1) {
             RVar::$nextVarId = 0; // TODO: This is not ideal. In very very very rare cases, it could lead to collisions.
@@ -31,13 +32,11 @@ class RVar extends ValuedQuery
         $this->setPositionalArg(0, new NumberDatum($this->id));
     }
 
-    protected function getTermType()
-    {
-        return TermTermType::PB_VAR;
+    protected function getTermType() {
+			return TermTermType::PB_VAR;
     }
 
-    public function getId()
-    {
-        return $this->id;
+    public function getId() {
+      return $this->id;
     }
 }
